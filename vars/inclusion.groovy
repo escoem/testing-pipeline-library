121dcd086d82ff7dffee1befb23e66045e91eb43
Emilio Escobar <escoem@users.noreply.github.com>
2019-02-08 10:19:39 +0100

    Create inclusion.groovy

---
+++ vars/inclusion.groovy
@@ -0,0 +1,9 @@
+// vars/inclusion.groovy
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
+    echo "I'm asking for a new library..."
+    library identifier: 'testing-pipeline-library-dynamic@crash', 
+            retriever: modernSCM([$class: 'GitSCMSource', remote: 'https://github.com/escoem/testing-pipeline-library.git'])
+}
