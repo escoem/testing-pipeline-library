26c2a6aa909eb160b92c4fde429ffff85856803d
Emilio Escobar <escoem@users.noreply.github.com>
2019-03-05 14:58:33 +0100

    Update kubernetesAgent.groovy

--- vars/kubernetesAgent.groovy
+++ vars/kubernetesAgent.groovy
@@ -1,8 +1,9 @@
 // vars/kubernetesAgent.groovy
 def call(Closure body) {
   def versions = libraryResource 'versions.properties'
-  def dockerImage = versions
-    
+  def props = readProperties text:versions
+  def dockerImage = props['maven']
+  echo "using $dockerImage maven docker image"
   def label = "mypod-${UUID.randomUUID().toString()}"
   podTemplate(label: label, containers: [
     containerTemplate(name: 'maven', image: dockerImage, ttyEnabled: true, command: 'cat')
