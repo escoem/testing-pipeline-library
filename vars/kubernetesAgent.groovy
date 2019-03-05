7f9e7d4b4fe7ba2f3d3fd30e34402e2fd5c40056
Emilio Escobar <escoem@users.noreply.github.com>
2019-03-05 10:53:53 +0100

    Update and rename inclusion.groovy to kubernetesAgent.groovy

---
+++ vars/kubernetesAgent.groovy
@@ -0,0 +1,12 @@
+// vars/kubernetesAgent.groovy
+def call(Closure body) {
+  def versions = libraryResource 'versions.properties'
+  def dockerImage = versions
+    
+  def label = "mypod-${UUID.randomUUID().toString()}"
+  podTemplate(label: label, containers: [
+    containerTemplate(name: 'maven', image: dockerImage, ttyEnabled: true, command: 'cat')
+    ]) {
+    body()
+  }
+}
